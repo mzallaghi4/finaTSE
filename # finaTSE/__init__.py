899e4653f3103a40d69309873525227add09d699
Mehdi Zallaghi <mmzallaghi@gmail.com>
2025-10-24 13:16:39 +0330

    Create __init__.py

---
+++ # finaTSE/__init__.py
@@ -0,0 +1,10 @@
+# finaTSE/__init__.py
+
+from .client import TSEClient
+
+__version__ = "0.1.2"
+__author__ = "MZA"
+__email__ = "mza@egmail.com"
+__license__ = "Apache License 2.0"
+
+__all__ = ["TSEClient"]
